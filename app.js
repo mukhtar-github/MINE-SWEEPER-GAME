5ab229f194e64c13f9b48531533e0e74fe556a97
mukhtar <mukhtartanimugarba@gmail.com>
2020-09-11 03:08:22 +0100

    Files updated

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid')
-    let width = 10
-    let squares = []
+    const grid = document.querySelector('.grid');
+    let width = 10;
+    let bombAmount = 20;
+    let squares = [];
 
     // Create Board
     function createBoard() {
+        //get shuffled game array with random bombs
+        const bombsArray = Array(bombAmount).fill('bomb');
+        const emptyArray = Array(width*width - bombAmount).fill('valid');
+        const gameArray = emptyArray.concat(bombsArray);
+        const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+
         for (let i = 0; i < width*width; i++) {
-            const square = document.createElement('div')
-            square.setAttribute('id', i)
-            grid.appendChild(square)
-            squares.push(square)
+            const square = document.createElement('div');
+            square.setAttribute('id', i);
+            grid.appendChild(square);
+            squares.push(square);
         }
     }
+    createBoard();
 
+    
 
-
-})
+});
