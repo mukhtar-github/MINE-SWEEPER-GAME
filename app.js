41905e0800172e18feb5a12015c2f27c1670058e
mukhtar <mukhtartanimugarba@gmail.com>
2020-09-12 08:46:14 +0100

    Updated file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Create Board
     function createBoard() {
-        //get shuffled game array with random bombs
+        //get shuffled game-array with random bombs
         const bombsArray = Array(bombAmount).fill('bomb');
         const emptyArray = Array(width*width - bombAmount).fill('valid');
         const gameArray = emptyArray.concat(bombsArray);
@@ -22,19 +22,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // add numbers
         for (let i = 0; i < squares.length; i++) {
+            let total = 0;
             const isLeftEdge = (i % width === 0);
             const isRigthtEdge = (i % width === width - 1);
+
+            if (squares[i].classList.contains('valid')) {
+                if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total ++;
+                if (i > 9 && !isRigthtEdge && squares[i + 1 - width].classList.contains('bomb')) total ++;
+                squares[i].setAttribute('data', total);
+                console.log(squares[i]);
+            }
         }
 
 
 
 
 
-
-
-
-
-        
     }
     createBoard();
 
