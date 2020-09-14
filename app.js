9db3e6a65c5ca13ed09936582147805b7a5eb5dc
mukhtar <mukhtartanimugarba@gmail.com>
2020-09-14 02:50:40 +0100

    Files updated

--- app.js
+++ app.js
@@ -29,6 +29,11 @@ document.addEventListener('DOMContentLoaded', () => {
             if (squares[i].classList.contains('valid')) {
                 if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb')) total ++;
                 if (i > 9 && !isRigthtEdge && squares[i + 1 - width].classList.contains('bomb')) total ++;
+                if (i > 10 && squares[i - width].classList.contains('bomb')) total ++;
+                if (i > 11 && !isLeftEdge && squares[i -1 - width].classList.contains('bomb')) total ++;
+                if (i < 98 && !isRigthtEdge && squares[i + 1].classList.contains('bomb')) total ++;
+                if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total ++;
+                
                 squares[i].setAttribute('data', total);
                 console.log(squares[i]);
             }
