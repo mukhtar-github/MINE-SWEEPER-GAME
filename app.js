7b75fbc1df381524450be8cde6e46e671a3617bd
mukhtar <mukhtartanimugarba@gmail.com>
2020-09-14 03:17:43 +0100

    Files updated

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
             square.classList.add(shuffledArray[i]);
             grid.appendChild(square);
             squares.push(square);
+
+            //normal click
+            square.addEventListener('click', function(e) {
+                click(square);
+            });
         }
 
         // add numbers
@@ -33,18 +38,21 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (i > 11 && !isLeftEdge && squares[i -1 - width].classList.contains('bomb')) total ++;
                 if (i < 98 && !isRigthtEdge && squares[i + 1].classList.contains('bomb')) total ++;
                 if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total ++;
-                
+                if (i < 88 && !isRigthtEdge && squares[i + 1 + width].classList.contains('bomb')) total ++;
+                if (i < 89 && squares[i + width].classList.contains('bomb')) total ++;
                 squares[i].setAttribute('data', total);
-                console.log(squares[i]);
             }
         }
-
-
-
-
-
     }
     createBoard();
+
+    // click on square actions
+    function click(square) {
+        if (square.classList.contains('bomb')) {
+            alert('Game Over');
+        }
+    }
+
 
 
 
